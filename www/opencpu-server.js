dacc40e321b4b1950291286c02aabd89bf5a47ca
baohongz <baohongz@gmail.com>
2020-07-14 09:05:03 -0400

    AWS Ohio

--- www/opencpu-server.js
+++ www/opencpu-server.js
@@ -1,4 +1,4 @@
 $(function() {
 //	ocpu.seturl("https://opencpu.bxgenomics.com/ocpu/library/QuickR/R");
-	ocpu.seturl("https://13.56.82.107/ocpu/library/QuickR/R");
+	ocpu.seturl("https://3.22.242.46/ocpu/library/QuickR/R");
 });
