986b4bac9615970bc7f79cf5d061c9de2268205e
baohongz <baohongz@gmail.com>
2020-07-13 23:06:16 -0400

    Amazon EC2

--- www/opencpu-server.js
+++ www/opencpu-server.js
@@ -1,3 +1,4 @@
 $(function() {
-	ocpu.seturl("https://opencpu.bxgenomics.com/ocpu/library/QuickR/R");
+//	ocpu.seturl("https://opencpu.bxgenomics.com/ocpu/library/QuickR/R");
+	ocpu.seturl("http://13.56.82.107/ocpu/library/QuickR/R");
 });
