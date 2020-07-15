cd57dcaf382df3f42abd3b583c1f0305e597a0d0
baohongz <baohongz@gmail.com>
2020-07-15 08:09:00 -0400

    Elastic IP

--- www/opencpu-server.js
+++ www/opencpu-server.js
@@ -1,4 +1,4 @@
 $(function() {
 //	ocpu.seturl("https://opencpu.bxgenomics.com/ocpu/library/QuickR/R");
-	ocpu.seturl("https://3.22.242.46/ocpu/library/QuickR/R");
+	ocpu.seturl("https://3.22.86.41/ocpu/library/QuickR/R");
 });
